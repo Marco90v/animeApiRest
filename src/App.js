cecef31fd7a9f1273455d5f6432a0b77089c2833
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2022-05-22 14:58:00 -0400

    add component recommendation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Top from './view/Top';
 import Watch from './view/Watch';
 import NoMatch from './view/NoMatch';
+import Recommendations from './view/Recommendation';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Top />} />
-          <Route path="watch" element={<Watch />} />
+          <Route path="/watch" element={<Watch />} />
+          <Route path="/recommendations" element={<Recommendations />} />
           <Route path="*" element={<NoMatch />} />
         </Routes>
       </main>
---
+++ src/view/Recommendation.js
@@ -0,0 +1,10 @@
+const Recommendations = () => {
+
+    return(
+        <>
+            <h2>Recommendation</h2>
+        </>
+    )
+}
+
+export default Recommendations
