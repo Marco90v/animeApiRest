6804d4121e7e54793ac5f4ebb6b4d1e7ce02c1f8
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2022-05-26 23:33:01 -0400

    cambio en el nombre del repositorio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer>
             <ul>
                 <li><a rel="noreferrer" href="https://github.com/Marco90v" target="_blank" >Github</a></li>
-                <li><a rel="noreferrer" href="https://github.com/Marco90v/animeList" target="_blank" >Repository</a></li>
+                <li><a rel="noreferrer" href="https://github.com/Marco90v/animeApiRest" target="_blank" >Repository</a></li>
                 <li><a rel="noreferrer" href="https://marco90v.github.io/Portafolio/" target="_blank" >Portfolio</a></li>
             </ul>
             <p>Author: Marco Velasquez Figarella</p>
