e382845e50d247159574c6011da6e5f2349eb30a
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2022-05-26 21:20:16 -0400

    enlaces a detalles

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Card = ({ anime }) => {
-    const { image_url , title , title_english , type, episode } = anime;
+    const { image_url , title , title_english , type, episode, mal_id } = anime;
     const nombre = title_english || title;
     const badge = type || episode;
     return(
         <li>
-            <picture>
-                <img src={ image_url } alt={ nombre } />
-            </picture>
-            <h3>{ title }</h3>
-            { badge && <p>{ badge }</p> }
+            <Link to={`details/${mal_id}`}>
+                <picture>
+                    <img src={ image_url } alt={ nombre } />
+                </picture>
+                <h3>{ title }</h3>
+                { badge && <p>{ badge }</p> }
+            </Link>
         </li>
     );
 }
